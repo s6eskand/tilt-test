869ec0db8d641ae85b7cefd0bcc7dca7c48f1210
Sam <sam.eskandar00@hotmail.com>
2020-05-24 03:02:02 -0400

    update state data types

--- src/Sections/Summoner.js
+++ src/Sections/Summoner.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import SummonerInfo from "./SummonerInfo";
+import { Link } from 'react-scroll';
 
-const API_KEY = 'RGAPI-0ffa8dda-d466-4992-9f3b-779d8bf4a5e2';
+const API_KEY = 'RGAPI-f790033b-ef0e-4995-a4a0-593af70c745c';
 
 class Summoner extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            allChampions: {},
+            mostPlayed: {},
             summonerNameInput: '',
             summonerName: '',
             profileIconId: 0,
             level: 0,
             summonerId: '',
             accountId: '',
+            rankedInfo: {},
+            matches: [],
+            opacity: '0',
         }
+    }
+
+    componentDidMount() {
+        fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
+            .then(res => res.json())
+            .then(res => {
+                const champions = res.data;
+                this.setState({allChampions: champions})
+            })
+            .catch(err => console.error(err));
     }
 
     handleSummonerChange = (e) => {
@@ -37,6 +54,37 @@ class Summoner extends React.Component {
             })
             .catch(err => console.error(err));
 
+        await fetch(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${this.state.summonerId}?api_key=${API_KEY}`)
+            .then(res => res.json())
+            .then(res => {
+                const mostPlayed = res[0];
+                this.setState({mostPlayed})
+            })
+            .catch(err => console.error(err));
+
+        await fetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${this.state.summonerId}?api_key=${API_KEY}`)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({rankedInfo: res[0]})
+            })
+            .catch(err => console.log(err));
+
+        await fetch(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${this.state.accountId}?queue=420&api_key=${API_KEY}`)
+            .then(res => res.json())
+            //.then(res => console.log(res))
+            .then(res => {
+                const matches = [];
+                for (let i = 0; i < 5; i++) {
+                    matches.push(res.matches[i]);
+                }
+                this.setState({matches})
+            })
+            .catch(err => console.error(err));
+
+        this.setState({
+            opacity: '1',
+        });
+
         console.log(this.state)
     };
 
@@ -50,16 +98,23 @@ class Summoner extends React.Component {
                         From there you’ll get advice on whether that next game is a great idea or if you should give the ranked climb a little break.
                     </h3>
                 </div>
-                <div className="submit">
+                <div className="submit" style={{zIndex: 20}}>
                     <input
                         onChange={this.handleSummonerChange.bind(this)}
                         value={this.state.summonerNameInput}
                         className="submit-input"
                         type="text"
                         placeholder="Your Summoner Name Here..."/>
-                    <button onClick={this.handleSubmit.bind(this)} className="submit-btn" >Tilt Test Me</button>
+                    <Link to="summonerInfo"><button onClick={this.handleSubmit.bind(this)} className="submit-btn" >Tilt Test Me</button></Link>
                 </div>
-
+                <SummonerInfo
+                    id="summonerInfo"
+                    opacity={this.state.opacity}
+                    name={this.state.summonerName}
+                    rank={this.state.rankedInfo}
+                    champions={this.state.mostPlayed}
+                    allChampions={this.state.allChampions}
+                />
             </div>
         )
     }
